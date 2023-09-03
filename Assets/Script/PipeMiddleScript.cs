efb2bf859a07baca84663f035a070900f1eaee19
kevin <zhengkevintao@gmail.com>
2023-09-03 21:36:31 +0200

    fix bug (multiple scores)

--- Assets/Script/PipeMiddleScript.cs
+++ Assets/Script/PipeMiddleScript.cs
@@ -6,6 +6,8 @@ public class PipeMiddleScript : MonoBehaviour
 {
     public LogicScript logic;
     // public connection between script, in this case LogicScript
+    private bool Bird1Collision = false;
+    private bool Bird2Collision = false;
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +16,37 @@ public class PipeMiddleScript : MonoBehaviour
         // find the connection
     }
 
+    public IEnumerator WaitBird1()
+    {
+        Bird1Collision = true;
+        yield return new WaitForSeconds(0.5f);
+        Bird1Collision = false;
+    }
+
+    public IEnumerator WaitBird2()
+    {
+        Bird2Collision = true;
+        yield return new WaitForSeconds(0.5f);
+        Bird2Collision = false;
+    }
+
     public void OnTriggerEnter2D(Collider2D collsion)
         // providing collision effect
     {
-        if (collsion.gameObject.layer == 3)
+        if (collsion.gameObject.layer == 3 && Bird1Collision == false)
         {
             logic.addScore(1);
+            WaitBird1();
+
         }
-        if (collsion.gameObject.layer == 7)
+        if (collsion.gameObject.layer == 7 && Bird2Collision == false)
         {
             logic.addScore2(1);
+            WaitBird2();
         }
         
         
     }
 }
+
+
